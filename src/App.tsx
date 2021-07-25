de365d2a372624c9e929edbfd6c86ae1bfb0e799
slify08 <islimaxmc@gmail.com>
2021-07-25 10:50:01 +0800

    add ui

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,33 @@ import { css, jsx, Global } from '@emotion/react';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Vret Boilerplate⚡</h1>
+      <a
+        href="https://github.com/joshxfi/vret-boilerplate"
+        target="_blank"
+        rel="noreferrer"
+      >
+        star this repo!
+      </a>
       <Global
         styles={css`
           * {
             margin: 0;
             padding: 0;
+          }
+
+          a {
+            text-decoration: none;
+            color: lime;
+          }
+
+          body {
+            text-align: left;
+            color: #fff;
+            background: #081229;
+            display: grid;
+            place-items: center;
+            height: 100vh;
           }
         `}
       />
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,1 @@
-// for interfaces
+// export interfaces
