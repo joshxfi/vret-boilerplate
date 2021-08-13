458b67de855ab764ce9ce300309322b7ba4e9f32
Josh Daniel <joshpersonal8@gmail.com>
2021-08-13 10:58:35 +0800

    add reusable media queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 /** @jsx jsx */
 import React from "react";
 import { css, jsx, Global } from "@emotion/react";
+import mq from "./config/breakpoints";
 import Maid from "css-maid";
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Vret Boilerplate⚡</h1>
-      <a
-        href='https://github.com/joshxfi/vret-boilerplate'
-        target='_blank'
-        rel='noreferrer'
-      >
+      <a href='https://github.com/joshxfi/vret-boilerplate' target='_blank' rel='noreferrer'>
         star this repo!
       </a>
       <Global
@@ -29,6 +26,24 @@ const App: React.FC = () => {
             display: grid;
             place-items: center;
             height: 100vh;
+          }
+
+          ${mq[2]} {
+            a {
+              color: firebrick;
+            }
+          }
+
+          ${mq[1]} {
+            a {
+              color: mediumblue;
+            }
+          }
+
+          ${mq[0]} {
+            a {
+              color: blanchedalmond;
+            }
           }
         `}
       />
---
+++ src/config/breakpoints.ts
@@ -0,0 +1,4 @@
+const breakpoints = ["768", "1024", "1280"];
+const mq = breakpoints.map(bp => `@media(max-width: ${bp}px)`);
+
+export default mq;
